6470954abcd314ec845caa6b926c1b27f87ff435
Igor Barros <igorbarrosrj@gmail.com>
2020-01-04 12:33:26 -0300

    Detele calc.js

--- calc.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $('#calc').click(function() {
-      const milhas = parseFloat($('#milhas').val());
-      const metros = 1609.34 * milhas;
-      $('#metros').val(metros);
-    })
-  })
